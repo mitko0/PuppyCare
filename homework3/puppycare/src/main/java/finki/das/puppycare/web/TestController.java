71d8a95b05e4870b524a37454e239b6293245343
mitko.lc <mitkomiloshovski@gmail.com>
2020-12-15 00:47:30 +0100

    use thymeleaf

--- homework3/puppycare/src/main/java/finki/das/puppycare/web/TestController.java
+++ homework3/puppycare/src/main/java/finki/das/puppycare/web/TestController.java
@@ -1,6 +1,7 @@
 package finki.das.puppycare.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 
     @GetMapping
-    public String test() {
+    public String test(Model model) {
+        model.addAttribute("attr1", "hello :D");
         return "welcome";
     }
 }
